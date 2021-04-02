97342f4e8ebca96ea9e9a73ab42450a963318839
MateoAlvarez-Dev <matheoz2003@gmail.com>
2021-04-02 16:30:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const { env } = require('node:process');
 const app = express();
 const server = require('http').createServer(app);
 
+app.get('/', (req, res) => {
+    res.send("Youre in the home page")
+})
+
+
 const PORT = env.PORT || 5510;
 server.listen(PORT , () => {
     console.log("Server started on port " + PORT);
